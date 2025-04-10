2cae7d91887058074a909fa03db945e6164ae70d
Jocelin Arriaga <jocelin.arriaga@yahoo.com>
2025-04-09 20:45:56 -0600

    html,css and js practice

--- javascript-lesson/profile-page.js
+++ javascript-lesson/profile-page.js
@@ -1,3 +1,7 @@
 // Select the button element
+const learnMoreButton = document.getElementById("learnMore");
 
 // Add an event listener to display a message when clicked
+learnMoreButton.addEventListener("click", () => {
+    alert("I hope you enjoyed learning about my hobby!");
+});
